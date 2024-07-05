d27a8c3a1ff60ece6975383243f5c3fb51ce5d70
Charanraju18 <charanraju925@gmail.com>
2024-07-05 14:12:05 +0530

    commit

--- JavaScript/survey_form.js
+++ JavaScript/survey_form.js
@@ -1,3 +1,24 @@
-function fun(event){
-	console.log(event.target.value);
+const Data = {
+	firstName : "",
+	secondName : "",
+	mobileNumber:"",
+	clg:""
+};
+
+function first_name(event){
+	Data.firstName=event.target.value
+}
+function last_name(event){
+	Data.secondName=event.target.value
+}
+function mobile_number(event){
+	Data.mobileNumber=event.target.value
+}
+function college(event){
+	Data.clg=event.target.value
+}
+
+function btn(event){
+	// event.preventdefault();
+	console.log(Data);
 }
