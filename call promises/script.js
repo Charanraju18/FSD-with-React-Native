f784097e06ff4154ac82a77908637e7747d62580
Charanraju18 <charanraju925@gmail.com>
2024-07-18 23:04:37 +0530

    commit

---
+++ call promises/script.js
@@ -0,0 +1,31 @@
+// var x = new Promise(function(success,failure){
+// 	// success('I got success first');
+// 	let x=1;
+// 	if(x>10){
+// 		success("X is greater than 10");
+// 	}
+// 	else{
+// 		failure("X is smaller than 10");
+// 	}
+// })
+// x.then(function(succes){
+// 	console.log(succes);
+// },function(error){
+// 	console.log(error);
+// })
+
+
+
+// async function fun(){
+// 	console.log("first");
+// 	setTimeout(function(){
+// 		console.log("hai");
+// 	},1)
+// 	console.log("second");
+// 	console.log("third");
+// }
+// fun();
+
+
+
+
